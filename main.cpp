6566ab913e54dd8da78d271bed6db1b7a6683d39
PYM <pymichau@eleves.ec-nantes.fr>
2014-12-01 10:26:11 +0100

    insertion du main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 1 décembre 2014, 10:05
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
