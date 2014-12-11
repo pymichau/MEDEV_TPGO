01cc93e06905310771f524afae5abda0dc365f2e
PYM <pymichau@eleves.ec-nantes.fr>
2014-12-11 09:56:11 +0100

    ajout d'un marqueur de prise

--- Joueur.cpp
+++ Joueur.cpp
@@ -34,27 +34,29 @@ void Joueur::jouer(Plateau& goban)
     
     while (fincoup==false)
     {
-    while (ordonnee>9 || ordonnee=<1) // pour empêcher la saisie de donnees incorrectes
-    {
-    std::cout<<"Sur quelle ligne souhaitez-vous poser votre pierre ?"<<std::endl;
-    std::cout<<"(indiquez un chiffre de 0 à "<<goban.getSize()<<")"<<std::endl;
-    std::cin>>ordonnee;
-    }
+        while (abscisse!='A' || abscisse!='B' || abscisse!='C' || abscisse!='D' || abscisse!='E' || abscisse!='F' || abscisse!='G' || abscisse!='H' || abscisse!='I' || abscisse!='J'..
+                .. abscisse!='a' || abscisse!='b' || abscisse!='c' || abscisse!='d' || abscisse!='e' || abscisse!='f' || abscisse!='g' || abscisse!='h' || abscisse!='i' || abscisse!='j')
+            {//la condition serait plus propre si je trouvais le moyen de dire abscisse "n'appartiens pas à l'ensemble"
+                std::cout<<std::endl<<"Sur quelle colonne souhaitez-vous poser votre pierre ?"<<std::endl;
+                std::cout<<"(indiquez une lettre de A à "<<goban.getSizeLettre()<<")"<<std::endl;
+                std::cin<<abscisse;
 
-    while (abscisse!='A' || abscisse!='B' || abscisse!='C' || abscisse!='D' || abscisse!='E' || abscisse!='F' || abscisse!='G' || abscisse!='H' || abscisse!='I' || abscisse!='J' )
-    {//la condition serait plus propre si je trouvais le moyen de dire abscisse "n'appartiens pas à l'ensemble"
-    std::cout<<std::endl<<"Sur quelle colonne souhaitez-vous poser votre pierre ?"<<std::endl;
-    std::cout<<"(indiquez une lettre de A à "<<goban.getSizeLettre()<<")"<<std::endl;
-    std::cin<<abscisse;
+                if (goban.getSize==5)//si l'utilisateur tape une lettre de G à J et que le tableau est de 5*5, la saisie est incorrecte, cette boucle permet d'en tenir compte
+                   {
+                        if (abscisse!='A' || abscisse!='B' || abscisse!='C' || abscisse!='D' || abscisse!='E' || abscisse!='F'..
+                            ..|| abscisse!='a' || abscisse!='b' || abscisse!='c' || abscisse!='d' || abscisse!='e' || abscisse!='f')
+                            {
+                                abscisse='z';
+                            }
+                    }
+            }
+        while (ordonnee>9 || ordonnee=<1) // pour empêcher la saisie de donnees incorrectes
+            {
+                std::cout<<"Sur quelle ligne souhaitez-vous poser votre pierre ?"<<std::endl;
+                std::cout<<"(indiquez un chiffre de 0 à "<<goban.getSize()<<")"<<std::endl;
+                std::cin>>ordonnee;
+            }
 
-    if (goban.getSize==5)//si l'utilisateur tape une lettre de G à J et que le tableau est de 5*5, la saisie est incorrecte, cette boucle permet d'en tenir compte
-    {
-        if (abscisse!='A' || abscisse!='B' || abscisse!='C' || abscisse!='D' || abscisse!='E' || abscisse!='F')
-        {
-           abscisse='z';
-        }
-    }
-    }
     // tester ici si le coup est valide -> test effectué dans la classe plateau
     //On transmet les données au plateau pour placer la pierre
     fincoup=goban.placerPierre(abscisse, ordonnee, m_couleur);
@@ -70,4 +72,14 @@ void Joueur::passer()
 bool Joueur::getPasse()
 {
     return m_passe;
+}
+
+void Joueur::setPrise(bool prise)
+{
+    m_prise=prise;
+}
+
+void Joueur::getPrise()
+{
+    return m_prise;
 }
--- Joueur.h
+++ Joueur.h
@@ -22,11 +22,14 @@ public:
     void passer();
     bool getPasse(); // au début de chaque tour, le plateau check si les joueurs ont passé, s'ils ont passé tous les deux l'un après l'autre, les deux passe sont à true et c'est la fin de la partie
     void capturePierre();
+    void setPrise(bool prise);
+    void getPrise();
     
 private:
     bool m_couleur;
     int m_nbPierreCapture;
     bool m_passe;
+    bool m_prise; //indique si le joueur a pris une pierre à ce tour
 
 };
 
